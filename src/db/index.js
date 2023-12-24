9384ddb40083cf73f22634f9984374351b3a7144
developerNitinsingh <nitinsingh09871@gmail.com>
2023-12-24 11:23:58 +0530

    write update controllers for users

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n Mongo DB connected !! DB HOST : ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("MONGODB connection FAILEDd", error);
-        process.exit(1)
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n 👍 Mongo DB connected !! DB HOST : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILEDd", error);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
